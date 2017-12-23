f4e8e3c4a2e6a3cb1080adf8d5f5e52039c9188b
Phuc Huynh <huynhhuuphuc0607@gmal.com>
2017-12-23 02:29:36 -0800

    Initial commit

---
+++ app/src/main/java/com/phuchuynh/projects/walkie/TrophyActivity.java
@@ -0,0 +1,13 @@
+package com.phuchuynh.projects.walkie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrophyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trophy);
+    }
+}
